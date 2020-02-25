d7c6ab1d5fab86c1965f914abbbab9d112b41bbd
Antonio <ameloni9@gmail.com>
2020-02-25 16:36:26 +0100

    Aggiunti livelli di difficoltà e qualche controllo

--- js/main.js
+++ js/main.js
@@ -5,32 +5,56 @@ La partita termina quando il giocatore inserisce un numero “vietato” o raggi
 Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.*/
 
 var numeriComputer = [];
+var livello = parseInt(prompt('Seleziona un livello di difficoltà. Scrivi 0 se vuoi giocare con numeri da 1 a 100, scrivi 1 per numeri da 1 a 80 o scrivi 2 per numeri da 1 a 50'));
+var difficolta;
+
+while (livello != 0 && livello != 1 && livello !=2) {
+    var livello = parseInt(prompt('Il numero da te inserito non è tra quelli indicati. Scrivi 0 se vuoi giocare con numeri da 1 a 100, scrivi 1 per numeri da 1 a 80 o scrivi 2 per numeri da 1 a 50'));
+}
+switch (livello) {
+    case 0:
+        difficolta = 100;
+        break;
+    case 1:
+        difficolta = 80;
+        break;
+    case 2:
+        difficolta = 50;
+        break;
+    default:
+        alert('Il numero da te inserito non è tra quelli indicati. Ricarica');
+        break;
+
+}
 
 for (var i = 0; i < 16; i++) {
-    numeriComputer[i] = generaRandom(1,100);
+    numeriComputer[i] = generaRandom(1,difficolta);
 }
 console.log(numeriComputer); //ovviamente da nascondere se si vuole giocare senza barare!
 
 var numeroUtente = [];
+var prova =[];
 
 for (var x = 0; x < 16; x++) {
-    numeroUtente[x] = parseInt(prompt('Inserisci un numero tra 1 e 100'));
-    while (numeroUtente[x]<1 || numeroUtente[x]>100) {
-        numeroUtente[x]= parseInt(prompt('Il numero inserito non è compreso tra 1 e 100. Inserisci nuovamente un numero tra 1 e 100.'));
+    numeroUtente[x] = parseInt(prompt('Inserisci un numero tra 1 e ' + difficolta));
+    while (numeroUtente[x]<1 || numeroUtente[x]>difficolta) {
+        numeroUtente[x]= parseInt(prompt('Il numero inserito non è compreso tra 1 e ' + difficolta + '. Inserisci nuovamente un numero tra 1 e ' + difficolta));
     }
     while (isNaN(numeroUtente[x])) {
-        numeroUtente[x] = parseInt(prompt('Non hai inserito un numero. Inserisci nuovamente un numero tra 1 e 100.'));
+        numeroUtente[x] = parseInt(prompt('Non hai inserito un numero. Inserisci nuovamente un numero tra 1 e ' + difficolta));
     }
+    while (prova.includes(numeroUtente[x])) {
+        numeroUtente[x]= parseInt(prompt('Hai già inserito questo numero. Inserisci nuovamente un numero tra 1 e ' + difficolta));
+    }
+    prova.push(numeroUtente[x]);
     if (numeriComputer.includes(numeroUtente[x])) {
         console.log('Addio, bomba!');
+        console.log('Il tuo punteggio è di: ' + (numeroUtente.length - 1));
         console.log('Ricarica la pagina per riprovare');
         break;
 
     }
-
 }
-
-console.log('Il tuo punteggio è di: ' + (numeroUtente.length - 1));
 
 
 function generaRandom(min, max) {
